18be5a32c24e1285384e2cbbe90e4b83d33ea362
pndnism <p.dai.man8@gmail.com>
2023-08-22 10:35:12 +0900

    remove generate-quiz.js

--- api/generate-quiz.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const apiKey = req.body.apiKey;
-  const category = req.body.category;
-
-  // OpenAI APIを呼び出してクイズを生成するロジック
-  // ...
-
-  res.status(200).json({ question, options, answer });
-}
